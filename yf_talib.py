fa78646647a05e39ca30e03559665e7276b4c08b
JvpCode7 <73199770+JvpCode7@users.noreply.github.com>
2023-01-16 15:53:16 -0500

    Se agregan comentarios

--- yf_talib.py
+++ yf_talib.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 21 10:13:47 2021
-
-@author: julia
+@author: julian
 """
 
 #pip install -i https://pypi.anaconda.org/ranaroussi/simple yfinance
@@ -14,32 +12,37 @@ Created on Sun Nov 21 10:13:47 2021
 import yfinance as yf
 import talib
 from talib import RSI, BBANDS
+import pandas as pd
 
-
+# Se carga la información de una accion desde Yahoo Finance a una Variable
 activo = yf.Ticker("AAPL")
 
-# get historical market data
+# Se trae la información de cierre diaria para un intervalo de 24 meses
 precios = activo.history(period="24mo", interval="1d")
 
 # Se debe trabaja con numpy arrys .to_numpy()
-
 preciosnp = precios.to_numpy()
 
+# Cada columna se convierte en una variable para clacular los indicadores técnicos
 Open = precios['Open'].to_numpy()
 high = precios['High'].to_numpy()
 low = precios['Low'].to_numpy()
 close = precios['Close'].to_numpy()
 
+# Se calculan promedios móviles de diferentes periodos
 SimpleMA20 = talib.SMA(close, timeperiod=20)
 SimpleMA40 = talib.SMA(close, timeperiod=40)
 SimpleMA50 = talib.SMA(close, timeperiod=50)
 SimpleMA100 = talib.SMA(close, timeperiod=100)
 SimpleMA200 = talib.SMA(close, timeperiod=200)
 
+# Se calcula el ADX
 adx = talib.ADX(high, low, close)
 
-
+# Se calculan las bandas de bollinger
 up, mid, low = BBANDS(close, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+
+# Se calcula las bandas de Bollinger
 rsi = RSI(close, timeperiod=14)
 
 # %B Bandas de Bollinger
@@ -48,10 +51,10 @@ def bbp(price):
     bbp = (close - low) / (up - low)
     return bbp
 
+# Se calcula el %B
 BollBeta = bbp(close)
 
-# armar el nuevo DataFrame
-
+# Se agrega al dataframe de precios todos los indicadores
 precios['SMA20'] = SimpleMA20.tolist()
 precios['SMA40'] = SimpleMA40.tolist()
 precios['SMA50'] = SimpleMA50.tolist()
@@ -63,43 +66,36 @@ precios['UP'] = up.tolist()
 precios['DOWN'] = low.tolist()
 precios['B'] = BollBeta.tolist()
 
-# Crear señales delas velas japonesas
+# TA-LIB también permite identificar señales de velas japonesas
 doji = talib.CDLDOJI(Open, high, low, close)
 
-# Charts 
-
+# Con la información se pueden crear gráficos usando matpltlib
 import matplotlib.pyplot as plt
 # pip install --upgrade mplfinance
 from mplfinance.original_flavor import candlestick_ohlc
-import pandas as pd
 import matplotlib.dates as mpl_dates #Módulo avanzado para fechas de gráficos Matplotlib
 
-
+# La fecha se establece como el índice
 precios['Date'] = precios.index
 
+# Se preprara el dataset para los parámetros de velas japonesas.
 ohlc = precios.loc[:, ['Date', 'Open', 'High', 'Low', 'Close']]
-
 ohlc['Date'] = pd.to_datetime(ohlc['Date'])
 ohlc['Date'] = ohlc['Date'].apply(mpl_dates.date2num)
 ohlc = ohlc.astype(float)
 
-# Creating Subplots
+# Crea Subplots
 fig, ax = plt.subplots()
-
 candlestick_ohlc(ax, ohlc.values, width=0.6, colorup='green', colordown='red', alpha=0.8)
-
-# Setting labels & titles
+# Colocando Labels y Títulos
 ax.set_xlabel('Date')
 ax.set_ylabel('Price')
 fig.suptitle('Daily Candlestick Chart of NIFTY50')
-
-# Formatting Date
+# Darle formato a la fecha
 date_format = mpl_dates.DateFormatter('%d-%m-%Y')
 ax.xaxis.set_major_formatter(date_format)
 fig.autofmt_xdate()
-
 fig.tight_layout()
-
 plt.show()
 
 
