598969ae543d21c27bb42c644d6afd08bb951cd7
JvpCode7 <73199770+JvpCode7@users.noreply.github.com>
2023-01-23 21:11:31 -0500

    Se completa función y código

--- bollAlert.py
+++ bollAlert.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 17 19:01:07 2023
-
-@author: julian
-"""
-
 import yfinance as yf
 import talib
 from talib import BBANDS
 import pandas as pd
+import numpy as np
 
 # Se carga la información de una accion desde Yahoo Finance a una Variable
 activo = yf.Ticker("AAPL")
@@ -21,7 +14,22 @@ close = precios['Close'].to_numpy()
 # Se calculan las bandas de bollinger
 up, mid, low = BBANDS(close, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
 
-
+# SE crea la función para la estrategia
+def alertaBoll(B, amplitud, alertaPercentil):
+    try:
+        if B > 1 and alertaPercentil < amplitud:
+            alerta = "SobreCompra"
+        elif B < 0 and alertaPercentil < amplitud:
+            alerta = "SobreVenta"
+        elif B > 1:
+            alerta = "Sobre Banda Superior sin volatilidad"
+        elif B < 0:
+            alerta = "Debajo Banda Inferior sin Volatilidad"
+        else:
+            alerta = "Sin Señal"
+        return alerta
+    except Exception as e:
+        return str(e)
 
 # %B Bandas de Bollinger
 def bbp(price):
@@ -49,3 +57,57 @@ precios = precios[precios['AMPLITUD'] < 1]
 
 precios['AMPLITUD'].plot()
 precios['AMPLITUD'].hist(bins=100)
+
+
+# Encontrar el percentil 90
+alertaPercentil = np.percentile(precios['AMPLITUD'], 90)
+
+precios['Alerta']  = precios.apply(lambda x: alertaBoll(x['B'], x['AMPLITUD'], alertaPercentil), axis=1)
+
+
+# Tomar los últimos 5 años para efectos de comportamiento del activo 
+#from datetime import datetime
+import datetime
+
+fechaLimite = datetime.datetime.now() - datetime.timedelta(days=5*365)
+fechaLimite = np.datetime64(fechaLimite)
+fechaLimite
+
+precios.dtypes
+precios['date'] = precios.index
+precios.dtypes
+precios['date'] = precios['date'].dt.tz_localize(None)
+precios.dtypes
+
+
+precios = precios[precios['date'] > fechaLimite]
+
+precios['AMPLITUD'].plot()
+precios['AMPLITUD'].hist(bins=100)
+
+
+# Encontrar el percentil 90
+alertaPercentil = np.percentile(precios['AMPLITUD'], 90)
+
+
+
+# Traer últimos 30 datos para caluclar parámetros de banda
+# Si el porcentaje B es negativo o superior a 1 y la amplitud es mayor al 95% se da alerta
+
+preciosHoy = activo.history(period="30d", interval="1d")
+close = preciosHoy['Close'].to_numpy()
+
+# Se calculan las bandas de bollinger
+up, mid, low = BBANDS(close, timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
+
+BollBeta = bbp(close)
+
+
+preciosHoy['UP'] = up.tolist()
+preciosHoy['MID'] = mid.tolist()
+preciosHoy['DOWN'] = low.tolist()
+preciosHoy['B'] = BollBeta.tolist()
+
+preciosHoy['AMPLITUD'] = preciosHoy['UP'] / preciosHoy['DOWN'] - 1
+
+preciosHoy['Alerta']  = preciosHoy.apply(lambda x: alertaBoll(x['B'], x['AMPLITUD'], alertaPercentil), axis=1)
